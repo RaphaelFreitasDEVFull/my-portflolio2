b2b7f034703f9352416c67a7ae57f2ef55edb5ad
RaphaelFreitasDEVFull <freitasraphael.dev@gmail.com>
2024-11-07 15:54:49 -0300

    fix: button

--- app/components/button/page.tsx
+++ app/components/button/page.tsx
@@ -1,5 +1,5 @@
-import { cn } from "@/app/lib/utils";
 import { ButtonHTMLAttributes } from "react";
+import { cn } from "@/app/lib/utils"; // Certifique-se de que a função 'cn' esteja sendo importada corretamente
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
